cd162731fbf99a46eda1fa6dc4d8bf566c2d426c
Sergey <arpenaz2000@gmail.com>
2023-03-27 13:38:16 +0300

    add Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же Маша");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
